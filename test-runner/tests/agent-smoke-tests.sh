a12f28dcfee3be21ca34ff007eb8f673edcbe961
Ian <ianmathieu@gmail.com>
2019-04-25 14:53:14 -0700

    Adding better syntax parsing

--- test-runner/tests/agent-smoke-tests.sh
+++ test-runner/tests/agent-smoke-tests.sh
@@ -10,35 +10,35 @@ PREFIX_CMD="docker exec ${CONTAINER_ID}"
 
 @test "iofog-agent network_interface" {
   result="$(${PREFIX_CMD} nano /etc/iofog-agent/config.xml | grep '<network_interface>dynamic</network_interface>' )"
-  [[ "${result} == *'dynamic'*" ]]
+  [[ "${result}"== *'dynamic'* ]]
 }
 
 @test "iofog-agent version" {
   result="$(${PREFIX_CMD} iofog-agent version)"
-  [[ "${result} == *'1.0.'*" ]]
+  [[ "${result}" == *'1.0.'* ]]
 }
 
 @test "iofog-agent info" {
   result="$(${PREFIX_CMD} iofog-agent info )"
-  [[ "${result} == *'Iofog UUID'*" ]]
+  [[ "${result}" == *'Iofog UUID'* ]]
 }
 
 @test "iofog-agent provision BAD" {
   result="$(${PREFIX_CMD} iofog-agent provision 'asd')"
-  [[ "${result} == *'Invalid Provisioning Key'*" ]]
+  [[ "${result}" == *'Invalid Provisioning Key'* ]]
 }
 
 @test "iofog-agent config INVALID RAM" {
   result="$(${PREFIX_CMD} iofog-agent config -m 50)"
-  [[ "${result} == *'Memory limit range'*" ]]
+  [[ "${result}" == *'Memory limit range'* ]]
 }
 
 @test "iofog-agent config RAM string" {
   result="$(${PREFIX_CMD} iofog-agent config -m test)"
-  [[ "${result} == *'invalid value'*" ]]
+  [[ "${result}" == *'invalid value'* ]]
 }
 
 @test "iofog-agent config VALID RAM" {
   result="$(${PREFIX_CMD} iofog-agent config -m 80)"
-  [[ "${result} == *'New Value'*" ]]
+  [[ "${result}" == *'New Value'* ]]
 }
