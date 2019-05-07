51ec2e2016e8c209c664ebb4f9124d89afe5886f
Todd Papaioannou <drluckyspin@gmail.com>
2019-05-07 00:18:24 -0700

    Minor code clean up.

--- run.sh
+++ run.sh
@@ -3,11 +3,15 @@
 #
 #!/usr/bin/env sh
 
+# Output the config for our Test suite config
 echo 'iofog-connector:8080' > conf/connector.conf
 echo 'iofog-controller:51121' > conf/controller.conf
 echo 'root@iofog-agent' > conf/agents.conf
+
 rm conf/id_ecdsa*
+
 ssh-keygen -t ecdsa -N "" -f conf/id_ecdsa -q
+
 cp conf/id_ecdsa.pub iofog-agent
 
 docker-compose -f docker-compose-test.yml pull test-runner
