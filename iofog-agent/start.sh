1c7423bd97142766ee846fdc7a7ead996cd517ed
SergeRadinovich <serge@edgeworx.io>
2019-05-15 15:14:20 +1200

    Bring up to date with develop

--- iofog-agent/start.sh
+++ iofog-agent/start.sh
@@ -18,22 +18,28 @@ if [ -f /first_run.tmp ]; then
         docker exec iofog-controller sh -c "ls /ready" > /dev/null 2>&1
     done
 
+    echo 'Logging in'
     login=$(curl --request POST \
         --url $CONTROLLER_HOST/user/login \
         --header 'Content-Type: application/json' \
         --data '{"email":"user@domain.com","password":"#Bugs4Fun"}')
+    echo "$login"
     token=$(echo $login | jq -r .accessToken)
 
+    echo 'Listing nodes'
     item=$(curl --request GET \
         --url $CONTROLLER_HOST/iofog-list \
         --header "Authorization: $token" \
         --header 'Content-Type: application/json')
+    echo "$item"
     uuid=$(echo $item | jq -r '.fogs[] | select(.name == "'"$NODE_NAME"'") | .uuid')
 
+    echo 'Provisioning'
     provisioning=$(curl --request GET \
         --url $CONTROLLER_HOST/iofog/$uuid/provisioning-key \
         --header "Authorization: $token" \
         --header 'Content-Type: application/json')
+    echo "$provisioning"
     key=$(echo $provisioning | jq -r .key)
 
     service iofog-agent start
---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -e
+
+# Output the config for our Test suite config
+echo 'iofog-connector:8080' > conf/connector.conf
+echo 'iofog-controller:51121' > conf/controller.conf
+echo 'root@iofog-agent' > conf/agents.conf
+
+docker-compose -f docker-compose-test.yml pull test-runner
+docker-compose -f docker-compose-test.yml up \
+    --build \
+    --abort-on-container-exit \
+    --exit-code-from test-runner \
+    --force-recreate \
+    --renew-anon-volumes
