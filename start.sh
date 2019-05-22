0e5d5a3a67b7c9102fe8e791426c9d1e86c07027
Todd Papaioannou <drluckyspin@gmail.com>
2019-05-22 02:06:35 -0700

    Refactored, cleaned up and added much better user output.

--- start.sh
+++ start.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env sh
+# start.sh - is the entry point to the Demo. It will setup an conf needed and spin up a docker-compose
+# environment of a fully configured ioFog ECN.
+#
+# Usage: start.sh
 
 set -e
 
+# Import our helper functions
+source utils.sh
+
+prettyHeader "Starting ioFog Demo environment"
+
+# Clean up any artifacts from a previous run
 rm conf/id_ecdsa* || true
+
+# Create a new ssh key and copy it into Agent
+echoInfo "Adding new ssh key pair to Agent"
 ssh-keygen -t ecdsa -N "" -f conf/id_ecdsa -q
 cp conf/id_ecdsa.pub iofog-agent
 
+# Spin up our Docker compose environment
+echoInfo "Building and Starting Docker Compose environment"
 docker-compose -f docker-compose.yml up --build --detach
+
+# Display the running environment
+docker ps
+echoNotify "ioFog Demo is now running"
--- stop.sh
+++ stop.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
+# stop.sh - shut down the Demo docker-compose environment
+#
+# Usage: stop.sh
 
 set -e
 
+# Import our helper functions
+source utils.sh
+
+prettyHeader "Spinning down iofog Demo environment"
 docker-compose down -v
+
+echoInfo "ioFog demo is stopped"
