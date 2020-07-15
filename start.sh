1cfd1aaa97dc8af2c64b0beab8419bf1fb32bc3f
Alex de Wergifosse <alex@edgeworx.io>
2020-07-15 12:30:48 +1200

    Update agent and controller images to use rc2

--- start.sh
+++ start.sh
@@ -85,8 +85,8 @@ startEnvironment() {
 
 ENVIRONMENT=''
 IOFOG_BUILD_NO_CACHE=''
-AGENT_IMAGE='docker.io/iofog/agent:2.0.0-beta'
-CONTROLLER_IMAGE='docker.io/iofog/controller:2.0.0-beta'
+AGENT_IMAGE='docker.io/iofog/agent:2.0.0-rc2'
+CONTROLLER_IMAGE='docker.io/iofog/controller:2.0.0-rc2'
 while [[ "$#" -ge 1 ]]; do
     case "$1" in
         -h|--help)
